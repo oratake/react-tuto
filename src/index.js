23dedf27051ab8fcd33fc86b4dda3ad63b04bd6d
oratake <smith.shimomura@gmail.com>
2021-05-03 00:45:11 +0900

    SquareをクリックするとonClickがcallされるように

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ class Square extends React.Component {
     return (
       <button
         className="square"
-        onClick={() => this.setState({value: 'X'})}
+        onClick={() => this.props.onClick()}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
@@ -30,7 +30,12 @@ class Board extends React.Component {
   }
 
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
