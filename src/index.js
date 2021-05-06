67e877f39339755fdc79431dd5c83332b4b4af13
oratake <smith.shimomura@gmail.com>
2021-05-06 20:16:27 +0900

    勝者確定時の表示追加

--- src/index.js
+++ src/index.js
@@ -44,7 +44,14 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    // const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (null !== winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
